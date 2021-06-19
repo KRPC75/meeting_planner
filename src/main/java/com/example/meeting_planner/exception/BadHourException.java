49cc59efcf46d1e9e908d0180fdd09029b5f9d7f
J20SE01 <alexandre.abonnet-ext@malakoffhumanis.com>
2021-06-19 09:42:30 +0200

    Correctifs exceptions

--- src/main/java/com/example/meeting_planner/exception/BadHourException.java
+++ src/main/java/com/example/meeting_planner/exception/BadHourException.java
@@ -1,7 +1,14 @@
 package com.example.meeting_planner.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
 public class BadHourException extends Exception {
+
+    private static final String MESSAGE = "L'heure choisi [%d] ne corresponds pas aux heures d'ouverture des salles (8h à 20h)";
+
     public BadHourException(Integer hour) {
-        super(String.format("L'heure choisi [%d] ne corresponds pas aux heures d'ouverture des salles (8h à 20h)", hour));
+        super(String.format(MESSAGE, hour));
     }
 }
--- src/main/java/com/example/meeting_planner/exception/NoRoomAvailableException.java
+++ src/main/java/com/example/meeting_planner/exception/NoRoomAvailableException.java
@@ -1,7 +1,14 @@
 package com.example.meeting_planner.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
 public class NoRoomAvailableException extends Exception {
+
+    private static final String MESSAGE = "Aucune salle disponible à ce jour correspondant aux critères d'entrée";
+
     public NoRoomAvailableException() {
-        super("Aucune salle disponible à ce jour");
+        super(MESSAGE);
     }
 }
